d167f5ac6764676c2b77ac3b880b8a6862eb34ad
Sel00k <sel00k@yandex.ru>
2025-07-25 04:04:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String arrowLeft  = "<--<<";
-        String arrowRight = ">>-->";
-        int lengthLeft  = arrowLeft.length();
-        int lengthRight = arrowRight.length();
         Scanner scanner = new Scanner( System.in );
-        System.out.printf( "Введите калиберду : " );
-        String text = scanner.next();
-        int index;
-        int poz = 0;
-        int countLeft  = 0;
-        int countRight = 0;
+        System.out.printf( "Введите строку : " );
+        String text = scanner.nextLine();
+        String[] words = text.toLowerCase().split( "\\s+" );
+        text.
+        for ( String word : words ){
+            char[] charArray = word.toCharArray();
+            Arrays.sort( charArray );
+            String str = new String( charArray );
 
-        while ( true ) {
-            index = text.indexOf( arrowLeft , poz );
-
-            if ( index == -1 ) {
-                break;
-            } else {
-                countLeft++;
-                poz = index + lengthLeft;
-            }
+            System.out.println( str );
         }
-
-        poz = 0;
-
-        while ( true ) {
-            index = text.indexOf( arrowRight , poz );
-
-            if ( index == -1 ) {
-                break;
-            } else {
-                countRight++;
-                poz += index + lengthRight;
-            }
-        }
-
-        System.out.println( arrowLeft + " : " + countLeft );
-        System.out.println( arrowRight + " : " + countRight );
 
         scanner.close();
     }
