102f26b1d706cf19835b8fc0634959418dec3579
Sel00k <sel00k@yandex.ru>
2025-07-29 00:56:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main( String[] args ) {
         Scanner scanner = new Scanner( System.in );
         String[] words;
-        String str;
 
         System.out.println( "### Сортировка символов в каждом слове строки ###" );
 
