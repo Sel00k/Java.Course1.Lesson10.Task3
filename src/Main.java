74c6e19b478ae965d9e52d349aa78d600a09012d
Sel00k <sel00k@yandex.ru>
2025-07-29 00:54:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,37 @@
+/* Задана строка, состоящая из букв английского алфавита, разделенных одним пробелом.
+   Необходимо каждую последовательность символов упорядочить по возрастанию
+   и вывести слова в нижнем регистре. */
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         Scanner scanner = new Scanner( System.in );
-        System.out.printf( "Введите строку : " );
-        String text = scanner.nextLine();
-        String[] words = text.toLowerCase().split( "\\s+" );
-        
-        for ( String word : words ){
-            char[] charArray = word.toCharArray();
-            Arrays.sort( charArray );
-            String str = new String( charArray );
+        String[] words;
+        String str , result;
 
-            System.out.println( str );
+        System.out.println( "### Сортировка символов в каждом слове строки ###" );
+
+        while ( true ) {
+            System.out.printf( "Введите строку (два слова разделённых пробелом) : " );
+            String text = scanner.nextLine();
+            words = text.toLowerCase().split( "\\s+" );   // Один или несколько (пробел или длинный пробел или табуляция)
+
+            if ( words.length == 2 ) break;
+
+            System.out.println( "Должно быть именно 2 слова!" );
         }
+
+        for ( int i = 0 ; i < words.length ; i++ ) {
+            char[] charArray = words[ i ].toCharArray();
+            Arrays.sort( charArray );
+            words[ i ] = new String( charArray );
+
+            System.out.println( words[ i ] );
+        }
+
+        System.out.println( "Получилось : " + String.join( " " , words ) );
 
         scanner.close();
     }
